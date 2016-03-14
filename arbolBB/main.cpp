b4d1e0d26cafb3fc5f9ec2f67f057a744279d6c6
Gerardo Cordero <cordero.gerard@gmail.com>
2016-03-14 13:47:12 -0430

    ArbolBB cargar desde archivos txt, siguiente paso combinar arbol a y b

--- arbolBB/main.cpp
+++ arbolBB/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 #include <fstream>
 #include "ArbolBB.cpp"
 #include "Arbol.cpp"
@@ -13,6 +14,8 @@ ArbolBB<int> arboluno;
 ArbolBB<int> arbola;
 ArbolBB<int> arbolb;
 
+void cargarADesdeTxt(ArbolBB<int>& arbol);
+void cargarBDesdeTxt(ArbolBB<int>& arbol);
 void inOrden(ArbolBB<int> &arbol, Apuntador raiz);
 
 
@@ -32,11 +35,12 @@ int main(int argc, char *argv[])
     	        cout << "\t3 .- Imprimir InOrden" << endl;
     	        cout << "Arbol Binario Segunda Entrega " << endl;
     	        cout << "-----------" << endl << endl;
-    	        cout << "\t4 .- Insertar en Arbol A" << endl;
-    	        cout << "\t4 .- Insertar en Arbol B" << endl;
-				cout << "\t5 .- Eliminar en Arbol Binario" << endl;
-				cout << "\t6 .- Imprimir InOrden" << endl;
-      cout << "\t0 .- Salir" << endl << endl;
+    	        cout << "\t4 .- Cargar en Arbol A desde a.txt" << endl;
+    	        cout << "\t5 .- Cargar en Arbol B desde b.txt" << endl;
+				cout << "\t6 .- Combinar Arbol A + B, Imprimir" << endl;
+				cout << "\t7 .- Combinar Arbol B + A, Imprimir" << endl;
+				cout << "\t8 .- Combinar Arbol A + B con Repetidos, Imprimir" << endl;
+				cout << "\t0 .- Salir" << endl << endl;
     	        cout << "Elije una opcion: ";
 
     	        cin >> opc;
@@ -57,13 +61,68 @@ int main(int argc, char *argv[])
 		cout << "Imprimir InOrden: " << endl;
 			 inOrden(arboluno,arboluno.ObtRaiz());
 				break;
+	case 4:
+			cout << "Cargar Arbol A: " << endl;
 
+					cargarADesdeTxt(arbola);
+					inOrden(arbola,arbola.ObtRaiz());
+					break;
+	case 5:
+			cout << "Cargar Arbol B: " << endl;
+
+					cargarBDesdeTxt(arbolb);
+					inOrden(arbolb,arbolb.ObtRaiz());
+					break;
+	case 6:
+			cout << "Combinar Arbol A + B, Imprimir: " << endl;
+
+					break;
+	case 7:
+			cout << "Combinar Arbol B + A, Imprimir: " << endl;
+
+					break;
+	case 8:
+			cout << "Combinar Arbol A + B con Repetidos, Imprimir: " << endl;
+
+					break;
 	}
     }while(opc!=0);
 
    
     system("PAUSE");
     return EXIT_SUCCESS;
+}
+
+void cargarADesdeTxt(ArbolBB<int>& arbol){
+
+	string line;
+	int ele;
+	ifstream myfile ("arbolBB/a.txt");
+	if(myfile.is_open()){
+		while(getline (myfile,line)){
+			ele = atoi(line.c_str());
+			 arbol.Insertar(ele);
+		}
+		myfile.close();
+	}else{
+		cout << "No puedo abrir txt";
+	}
+}
+void cargarBDesdeTxt(ArbolBB<int>& arbol){
+
+	string line;
+	int ele;
+	ifstream myfile ("arbolBB/b.txt");
+	if(myfile.is_open()){
+		while(getline (myfile,line)){
+			ele = atoi(line.c_str());
+			 arbol.Insertar(ele);
+		}
+		myfile.close();
+	}else{
+		cout << "No puedo abrir txt";
+	}
+
 }
 
 void inOrden(ArbolBB<int>& arbol, Nodo<int>* nodo){
